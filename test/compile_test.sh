b9061aae7fc3d4e8301fe19d0cbf8a701968dd16
h8rry <harry@harrychen.net>
2016-07-03 22:43:16 +0800

    add compile test

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -1,2 +1,10 @@
-compile
-assertCapturedSuccess
+#!/bin/sh
+
+. ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
+
+testCompile()
+{
+  capture ${BUILDPACK_HOME}/bin/compile ${BUILD_DIR} ${CACHE_DIR}
+  assertEquals 0 ${rtrn}
+  assertEquals "" "`cat ${STD_ERR}`"
+}
